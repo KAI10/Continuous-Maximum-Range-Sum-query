d1e74b6d50fbdf16aa6afca3a60393019e959d81
Kazi Ashik Islam <ikaziashik@gmail.com>
2016-10-08 18:34:19 +0600

    utilities, co_maxrs start

---
+++ cpp/co_maxrs.cpp
@@ -0,0 +1,19 @@
+/*
+ * co_maxrs.cpp
+ * main program
+ *
+ * Created by: Ashik <ashik@KAI10>
+ * Created on: 2016-09-10
+ */
+
+#include <bits/stdc++.h>
+#include "objects.h"
+#include "utilities.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ cpp/utilities.h
@@ -0,0 +1,26 @@
+/*
+ * utilities.h
+ *
+ * necessary functions
+ *
+ * Created by: Ashik <ashik@KAI10>
+ * Created on: 2016-09-10
+ */
+
+const int STEP = 1;
+const double r_w = 1000;
+const double r_h = 1000;
+const double a_w = 100;
+const double a_h = 100;
+
+Area coverage(r_h, r_w);
+Area area(a_h, a_w);
+
+/*
+void display()
+{
+    FILE *fp = fopen("output.txt", "w");
+
+}
+*/
+
